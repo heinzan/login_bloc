c12f00ab24b0490541cf38b91ea63ba09dab073f
hien aung zan <hein.az@blueplanet.com.mm>
2019-02-26 01:19:10 +0630

    [update]loginscreen ui

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+ Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Adress'
+      )
+    );
+ }
+
+  Widget passwordField() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password'
+      ),
+
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Log In'),
+      color: Colors.amberAccent,
+      onPressed: (){
+
+      },
+    );
+  }
+
+}
